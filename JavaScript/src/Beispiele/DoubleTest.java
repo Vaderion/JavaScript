46911d6370ef1ed58cfaff8d1f031938035a97a1
DanielVaderion <DanielVaderion@Vaderions-PC.Speedport_W_724V_Typ_A_05011603_05_020>
2017-08-01 00:29:47 +0200

    3.1.5

---
+++ JavaScript/src/Beispiele/DoubleTest.java
@@ -0,0 +1,14 @@
+package Beispiele;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		double x = 325e200;
+		double y = 325e-200;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(x+y);
+		System.out.println(x+100000);
+	}
+
+}
---
+++ JavaScript/src/Beispiele/Rounded.java
@@ -0,0 +1,21 @@
+package Beispiele;
+
+public class Rounded {
+
+	public static void main(String[] args) {
+		System.out.println(8f);
+		System.out.println(88f);
+		System.out.println(888f);
+		System.out.println(8888f);
+		System.out.println(88888f);
+		System.out.println(888888f);
+		System.out.println(8888888f);
+		System.out.println(88888888f);
+		System.out.println(888888888f);
+		System.out.println(8888888888f);
+		System.out.println(88888888888f);
+		
+		System.out.println(1f+1000000000000f-1000000000000f);
+	}
+
+}
